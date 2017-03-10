20f8e919316d42be9c7687f028f792a42889a0a7
Teerapong Manakul <teerapong.m@zanrook.com>
2017-03-11 00:27:40 +0700

    Remove Mobirise css

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,7 +1,6 @@
 import { Bert } from 'meteor/themeteorchef:bert';
 import 'superslides/dist/jquery.superslides.min.js';
 import 'superslides/dist/stylesheets/superslides.css';
-import 'mobirise/assets/animate.css/animate.min.css';
 import './routes.js';
 
 Bert.defaults.style = 'growl-top-right';
