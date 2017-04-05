401f35e93b5fc685610674e1c9c046bc72adf8e5
Teerapong Manakul <teerapong.m@zanrook.com>
2017-04-06 02:28:59 +0700

    Remove course module

--- imports/ui/components/CourseEditor.js
+++ imports/ui/components/CourseEditor.js
@@ -2,11 +2,57 @@
 
 import React from 'react';
 import { FormGroup, Label, Input, Button } from 'reactstrap';
-import courseEditor from '../../modules/course-editor';
+import { browserHistory } from 'react-router';
+import { Bert } from 'meteor/themeteorchef:bert';
+import { upsertCourse } from '../../api/courses/methods';
+import $ from '../../modules/validation';
+
+const handleUpsert = (component) => {
+  const { doc } = component.props;
+  const confirmation = doc && doc._id ? 'Course updated!' : 'Course added!';
+  const upsert = {
+    title: document.querySelector('[name="title"]').value.trim(),
+    body: document.querySelector('[name="body"]').value.trim(),
+  };
+
+  if (doc && doc._id) upsert._id = doc._id;
+
+  upsertCourse.call(upsert, (error, response) => {
+    if (error) {
+      Bert.alert(error.reason, 'danger');
+    } else {
+      component.editor.reset();
+      Bert.alert(confirmation, 'success');
+      browserHistory.push(`/courses/${response.insertedId || doc._id}`);
+    }
+  });
+};
+
+const validate = (component) => {
+  $(component.editor).validate({
+    rules: {
+      title: {
+        required: true,
+      },
+      body: {
+        required: true,
+      },
+    },
+    messages: {
+      title: {
+        required: 'Need a title in here, Seuss.',
+      },
+      body: {
+        required: 'This thneeds a body, please.',
+      },
+    },
+    submitHandler() { handleUpsert(component); },
+  });
+};
 
 export default class CourseEditor extends React.Component {
   componentDidMount() {
-    courseEditor({ component: this });
+    validate(this);
     setTimeout(() => { document.querySelector('[name="title"]').focus(); }, 0);
   }
 
